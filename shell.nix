2dde66e6c71d5b6856fc76eed18ba7ceac450a0c
Chris S <chris@soundshare.co.uk>
2019-11-03 17:27:58 +0000

    nix attempt

---
+++ shell.nix
@@ -0,0 +1,16 @@
+{
+  nixpkgs ? import <nixpkgs> {}
+, sources ? import ./nix/sources.nix
+, compiler ? "ghc865" } :
+let
+  niv = import sources.nixpkgs {
+    overlays = [
+      (_ : _ : { niv = import sources.niv {}; })
+    ] ;
+    config = {};
+  };
+  pkgs = niv.pkgs;
+  myHaskellPackages = pkgs.haskell.packages.${compiler}.override {
+  };
+in
+myHaskellPackages.callCabal2nix "moscoviumorange" (./.) {}
