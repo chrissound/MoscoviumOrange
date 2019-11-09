aedb81b7a00a81764c49332d885f0267617f2bf0
Chris S <chris@soundshare.co.uk>
2019-11-09 19:00:35 +0000

    nix default

---
+++ default.nix
@@ -0,0 +1,15 @@
+{
+  sources ? import ./nix/sources.nix
+, compiler ? "ghc865" } :
+let
+  niv = import sources.nixpkgs {
+    overlays = [
+      (_ : _ : { niv = import sources.niv {}; })
+    ] ;
+    config = {};
+  };
+  pkgs = niv.pkgs;
+  myHaskellPackages = pkgs.haskell.packages.${compiler}.override {
+  };
+in
+(myHaskellPackages.callCabal2nix "moscoviumorange" (./.) {})
