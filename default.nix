ebf08fe9e1aafd60643321fe98a899bc46b1d66e
Chris S <chris@soundshare.co.uk>
2019-11-17 19:06:09 +0000

    Nixify with my own rainbox for performance fix thingy. Stackify too.

--- default.nix
+++ default.nix
@@ -10,6 +10,14 @@ let
   };
   pkgs = niv.pkgs;
   myHaskellPackages = pkgs.haskell.packages.${compiler}.override {
+    overrides = self: super: rec {
+      rainbox = self.callCabal2nix "rainbox" (builtins.fetchTarball "https://github.com/chrissound/rainbox/tarball/nix") {};
+      rainbow = self.callCabal2nix "rainbow" (builtins.fetchGit {
+        url = "git@github.com:massysett/rainbow.git";
+        rev = "852c6b23e570ebcb190d9169a3aa24449c70e33f";
+      })
+      {};
+    };
   };
 in
-(myHaskellPackages.callCabal2nix "moscoviumorange" (./.) {})
+(myHaskellPackages.callCabal2nixWithOptions "moscoviumorange" (./.) "--enable-profiling"{})
