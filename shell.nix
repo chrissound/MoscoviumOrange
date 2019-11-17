fe514cb5f46ed015424937e128a2ba40e0027f8b
Chris S <chris@soundshare.co.uk>
2019-11-17 20:26:28 +0000

    nixify

--- shell.nix
+++ shell.nix
@@ -1,15 +1,1 @@
-{
-  sources ? import ./nix/sources.nix
-, compiler ? "ghc865" } :
-let
-  niv = import sources.nixpkgs {
-    overlays = [
-      (_ : _ : { niv = import sources.niv {}; })
-    ] ;
-    config = {};
-  };
-  pkgs = niv.pkgs;
-  myHaskellPackages = pkgs.haskell.packages.${compiler}.override {
-  };
-in
-(myHaskellPackages.callCabal2nix "moscoviumorange" (./.) {}).env
+args@{...}: (import ./default.nix args).env
