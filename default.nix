44e7aad78f88d6bff53aa399eaa990cb765f05db
Chris S <chris@soundshare.co.uk>
2022-07-28 19:58:57 +0100

    Fix deleted github repo reference

--- default.nix
+++ default.nix
@@ -12,11 +12,12 @@ let
   myHaskellPackages = pkgs.haskell.packages.${compiler}.override {
     overrides = self: super: rec {
       rainbox = self.callCabal2nix "rainbox" (builtins.fetchTarball "https://github.com/chrissound/rainbox/tarball/nix") {};
-      rainbow = self.callCabal2nix "rainbow" (builtins.fetchGit {
-        url = "git@github.com:massysett/rainbow.git";
-        rev = "852c6b23e570ebcb190d9169a3aa24449c70e33f";
-      })
-      {};
+      rainbow = self.callCabal2nix "rainbow" (builtins.fetchTarball "https://hackage.haskell.org/package/rainbow-0.34.2.2/rainbow-0.34.2.2.tar.gz") {};
+      #rainbow = self.callCabal2nix "rainbow" (builtins.fetchGit {
+        #url = "git@github.com:massysett/rainbow.git";
+        #rev = "852c6b23e570ebcb190d9169a3aa24449c70e33f";
+      #})
+      #{};
     };
   };
 in
